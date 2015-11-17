d5717131a1c682a18027234a5cd191d9a219e5a0
Eric Giovanola <ericgio@gmail.com>
2015-11-17 03:54:08 -0800

    Update package.json + move index.js

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as TokenizerInput } from './TokenizerInput.react';
+export { default as Typeahead } from './Typeahead.react';
+export { default as TypeaheadInput } from './TypeaheadInput.react';
