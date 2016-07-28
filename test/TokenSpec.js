df5ac26ba2b1dd61c821c266c20e001e45b2de2f
Eric Giovanola <ericgio@gmail.com>
2016-07-28 00:42:06 -0700

    Basic test for Token component

---
+++ test/TokenSpec.js
@@ -0,0 +1,38 @@
+import {noop} from 'lodash';
+import React from 'react';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
+
+import Token from '../src/Token.react';
+
+describe('<Token>', () => {
+  const basicToken = <Token>Basic</Token>;
+
+  it('renders a basic token', () => {
+    const instance = ReactTestUtils.renderIntoDocument(basicToken);
+    const tokenNode = ReactTestUtils.findRenderedDOMComponentWithClass(
+      instance,
+      'token'
+    );
+
+    expect(tokenNode).to.exist;
+  });
+
+  it('renders a removeable token', () => {
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Token onRemove={noop}>
+        Removeable
+      </Token>
+    );
+    const tokenNode = ReactTestUtils.findRenderedDOMComponentWithClass(
+      instance,
+      'token-removeable'
+    );
+    const closeButtonNode = ReactTestUtils.findRenderedDOMComponentWithClass(
+      instance,
+      'close-button'
+    );
+
+    expect(tokenNode).to.exist;
+    expect(closeButtonNode).to.exist;
+  });
+});
