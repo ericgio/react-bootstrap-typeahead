b23cb4cbb014c06765019a9e4c66bdf14a621d1c
Eric Giovanola <ericgio@gmail.com>
2016-07-28 00:56:37 -0700

    Add basic TokenizerInput test

---
+++ test/TokenizerInputSpec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
+
+import TokenizerInput from '../src/TokenizerInput.react';
+
+import states from '../example/exampleData';
+
+describe('<TokenizerInput>', () => {
+
+  it('renders a TokenizerInput', () => {
+    const instance = ReactTestUtils.renderIntoDocument(
+      <TokenizerInput
+        options={[]}
+        selected={[]}
+        text=""
+      />
+    );
+    const inputNode = ReactTestUtils.findRenderedDOMComponentWithClass(
+      instance,
+      'bootstrap-tokenizer'
+    );
+
+    expect(inputNode).to.exist;
+  });
+
+  it('renders tokens in the tokenizer', () => {
+    const instance = ReactTestUtils.renderIntoDocument(
+      <TokenizerInput
+        options={states}
+        selected={states.slice(0, 3)}
+        text=""
+      />
+    );
+    const tokens = ReactTestUtils.scryRenderedDOMComponentsWithClass(
+      instance,
+      'token'
+    );
+
+    expect(tokens.length).to.equal(3);
+  });  
+
+});
