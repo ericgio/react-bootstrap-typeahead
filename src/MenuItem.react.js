b42ffa2f4aeba50e14f3cbe47c3b8e65fa2a4ce5
John Blossom <jblossom@medianewsgroup.com>
2016-10-24 14:39:21 -0600

    PR 97 review: use role=button

--- src/MenuItem.react.js
+++ src/MenuItem.react.js
@@ -33,7 +33,7 @@ const MenuItem = React.createClass({
           'active': active,
           'disabled': disabled,
         }, className)}>
-        <a style={{ cursor: 'pointer' }} onClick={this._handleClick}>
+        <a role="button" onClick={this._handleClick}>
           {children}
         </a>
       </li>
