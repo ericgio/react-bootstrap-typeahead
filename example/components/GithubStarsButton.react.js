f24040edd03123ff735e2701309410d24f91b99d
Eric Giovanola <ericgio@gmail.com>
2017-04-26 22:22:12 -0700

    Fix deprecation warning for Github button on examples page

--- example/components/GithubStarsButton.react.js
+++ example/components/GithubStarsButton.react.js
@@ -14,9 +14,9 @@ const GitHubStarsButton = React.createClass({
       <a
         aria-label={`Star ${AUTHOR_REPO} on GitHub`}
         className="github-button"
-        data-count-api={`/repos/${AUTHOR_REPO}#stargazers_count`}
         data-count-aria-label="# stargazers on GitHub"
         data-count-href={`/${AUTHOR_REPO}/stargazers`}
+        data-show-count={true}
         href={`https://github.com/${AUTHOR_REPO}`}>
         Star
       </a>
