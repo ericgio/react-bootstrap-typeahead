a10e50c1c29a5b4c336a3c8bac9cfe75cde20402
Eric Giovanola <ericgio@gmail.com>
2017-07-20 21:58:40 -0700

    Add cleanup script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# Delete existing dist & lib files
+./scripts/cleanup.sh
+
 # Build minified standalone version in dist
-rm -rf dist
 ./node_modules/.bin/webpack --config webpack/webpack.config.js
 ./node_modules/.bin/webpack --config webpack/webpack.config.prod.js
 
 # Build ES5 modules to lib
-rm -rf lib
 ./node_modules/.bin/babel src --out-dir lib
 
 # Build example file
