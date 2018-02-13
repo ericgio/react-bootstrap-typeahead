ff6c4642b4eba6959c9c331a1084a5402d50bb05
Eric Giovanola <ericgio@gmail.com>
2018-02-13 15:59:14 -0800

    Add sinon and convert MenuItemSpec

--- test/MenuItemSpec.js
+++ test/MenuItemSpec.js
@@ -1,98 +1,103 @@
 import {expect} from 'chai';
+import {mount, shallow} from 'enzyme';
+import {noop} from 'lodash';
+import PropTypes from 'prop-types';
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
+import sinon from 'sinon';
 
 import MenuItem, {BaseMenuItem} from '../src/MenuItem';
-import TypeaheadContext from './utils/TypeaheadContext';
-import {scryMenuItems} from './testUtils';
 
-function getBaseMenuItemNode(props={}) {
-  const instance = ReactTestUtils.renderIntoDocument(
-    <BaseMenuItem {...props}>
-      This is a base menu item.
-    </BaseMenuItem>
-  );
-  return scryMenuItems(instance)[0];
-}
+const event = {
+  preventDefault: noop,
+};
 
-function getMenuItemNode(props={}, contextProps={}) {
-  const instance = ReactTestUtils.renderIntoDocument(
-    <TypeaheadContext {...contextProps}>
-      <MenuItem {...props} option={{label: 'test'}} position={0}>
-        This is a menu item.
-      </MenuItem>
-    </TypeaheadContext>
-  );
-  return scryMenuItems(instance)[0];
-}
-
-describe('MenuItemSpec', () => {
-  let clickValue;
-
-  function onClick() {
-    clickValue = 'clicked';
-  }
+describe('<BaseMenuItem>', () => {
+  let baseMenuItem, onClick;
 
   beforeEach(() => {
-    clickValue = undefined;
+    onClick = sinon.spy();
+    baseMenuItem = shallow(
+      <BaseMenuItem onClick={onClick}>
+        This is a base menu item.
+      </BaseMenuItem>
+    );
   });
 
-  describe('<BaseMenuItem>', () => {
-    it('renders a base menu item', () => {
-      const menuItemNode = getBaseMenuItemNode();
-      expect(menuItemNode).to.exist;
-    });
-
-    it('renders an active base menu item', () => {
-      const menuItemNode = getBaseMenuItemNode({active: true});
-      expect(menuItemNode.className).to.contain('active');
-    });
-
-    it('triggers an event when clicked', () => {
-      const menuItemNode = getBaseMenuItemNode({onClick});
-      ReactTestUtils.Simulate.click(menuItemNode.childNodes[0]);
-
-      expect(clickValue).to.equal('clicked');
-    });
-
-    it('renders a disabled base menu item', () => {
-      const menuItemNode = getBaseMenuItemNode({disabled: true});
-      expect(menuItemNode.className).to.contain('disabled');
-
-      // Disabled items don't allow `onClick` events.
-      ReactTestUtils.Simulate.click(menuItemNode.childNodes[0]);
-      expect(clickValue).to.equal(undefined);
-    });
-
+  it('renders a base menu item', () => {
+    expect(baseMenuItem).to.exist;
+    expect(baseMenuItem.type()).to.equal('li');
   });
 
-  describe('<MenuItem>', () => {
-
-    it('renders a menu item', () => {
-      const menuItemNode = getMenuItemNode();
-      expect(menuItemNode).to.exist;
-    });
-
-    it('changes the active state of the menu item', () => {
-      const inactiveNode = getMenuItemNode();
-      expect(inactiveNode.className).to.not.contain('active');
-
-      const activeNode = getMenuItemNode({}, {activeIndex: 0});
-      expect(activeNode.className).to.contain('active');
-    });
-
-    it('sets the active state if it is the only result', () => {
-      const activeNode = getMenuItemNode({}, {isOnlyResult: true});
-      expect(activeNode.className).to.contain('active');
-    });
-
-    it('triggers an event when clicked', () => {
-      const menuItemNode = getMenuItemNode({onClick});
-      ReactTestUtils.Simulate.click(menuItemNode.childNodes[0]);
-
-      expect(clickValue).to.equal('clicked');
-    });
-
+  it('renders an active base menu item', () => {
+    baseMenuItem.setProps({active: true});
+    expect(baseMenuItem.hasClass('active')).to.equal(true);
   });
 
+  it('triggers an event when clicked', () => {
+    baseMenuItem.find('a').simulate('click', event);
+    expect(onClick.calledOnce).to.equal(true);
+  });
+
+  it('renders a disabled base menu item', () => {
+    baseMenuItem.setProps({disabled: true});
+    baseMenuItem.find('a').simulate('click', event);
+
+    expect(baseMenuItem.hasClass('disabled')).to.equal(true);
+    expect(onClick.notCalled).to.equal(true);
+  });
+
+});
+
+describe('<MenuItem>', () => {
+  let menuItem, onClick;
+
+  const context = {
+    activeIndex: -1,
+    isOnlyResult: false,
+    onActiveItemChange: noop,
+    onInitialItemChange: noop,
+    onMenuItemClick: noop,
+  };
+
+  const childContextTypes = {
+    activeIndex: PropTypes.number.isRequired,
+    isOnlyResult: PropTypes.bool.isRequired,
+    onActiveItemChange: PropTypes.func.isRequired,
+    onInitialItemChange: PropTypes.func.isRequired,
+    onMenuItemClick: PropTypes.func.isRequired,
+  };
+
+  beforeEach(() => {
+    onClick = sinon.spy();
+    menuItem = mount(
+      <MenuItem onClick={onClick} option={{label: 'test'}} position={0}>
+        This is a menu item.
+      </MenuItem>,
+      {childContextTypes, context}
+    );
+  });
+
+  it('renders a menu item', () => {
+    expect(menuItem).to.exist;
+    expect(menuItem.find('a')).to.have.length(1);
+  });
+
+  it('changes the active state of the menu item', () => {
+    expect(menuItem.hasClass('active')).to.equal(false);
+
+    menuItem.setContext({activeIndex: 0});
+    expect(menuItem.find('a').hasClass('active')).to.equal(true);
+  });
+
+  it('sets the active state if it is the only result', () => {
+    expect(menuItem.hasClass('active')).to.equal(false);
+
+    menuItem.setContext({isOnlyResult: true});
+    expect(menuItem.find('a').hasClass('active')).to.equal(true);
+  });
+
+  it('triggers an event when clicked', () => {
+    menuItem.find('a').simulate('click', event);
+    expect(onClick.calledOnce).to.equal(true);
+  });
 });
