3628a7adab998091c0812f5e71e212bc17129885
Eric Giovanola <ericgio@gmail.com>
2018-02-15 15:53:58 -0800

    Add minified CSS to build script (#307)

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,12 @@
 # Delete existing dist & lib files
 ./scripts/cleanup.sh
 
-# Compile SCSS file
+# Compile SCSS file to development and prod CSS files
 ./node_modules/node-sass/bin/node-sass scss/Typeahead.scss css/Typeahead.css \
 	--output-style expanded
+
+./node_modules/node-sass/bin/node-sass scss/Typeahead.scss css/Typeahead.min.css \
+  --output-style compressed
 
 # Build minified standalone version in dist
 ./node_modules/.bin/webpack --config webpack/webpack.config.js
