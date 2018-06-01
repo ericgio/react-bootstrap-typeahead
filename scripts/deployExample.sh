1c108e6890237f6feaa7db0d609085959d116510
Eric Giovanola <ericgio@gmail.com>
2018-06-01 10:51:13 -0700

    Fix examples deploy script

--- scripts/deployExample.sh
+++ scripts/deployExample.sh
@@ -1,6 +1,6 @@
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-./node_modules/gh-pages/bin/gh-pages \
+./node_modules/gh-pages/bin/gh-pages.js \
   -d example/ \
   -s '{index.html,package-example.js,assets/*}' \
   -m "v${PACKAGE_VERSION}"
