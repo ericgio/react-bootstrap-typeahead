ada61dcb326879d8112cc3c4d3bd230515c0dd2d
Eric Giovanola <ericgio@gmail.com>
2018-09-13 14:38:49 -0700

    Fix console warning in test

--- test/components/TypeaheadInputSingleSpec.js
+++ test/components/TypeaheadInputSingleSpec.js
@@ -18,6 +18,7 @@ describe('<TypeaheadInputSingle>', () => {
         inputRef={noop}
         labelKey="name"
         onAdd={noop}
+        onChange={noop}
         onClear={noop}
         onFocus={noop}
         options={options}
