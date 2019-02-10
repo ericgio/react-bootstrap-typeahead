ericgio/react-bootstrap-typeahead

commit 5c9444828ef2fc127fd3e6461a005938b7d6cd35

Use PureComponent in Highlighter to avoid unnecessary re-renders

src/Highlighter.react.js
@@ -9,7 +9,7 @@ import {getMatchBounds} from './utils';
  * Results are already filtered by the time the component is used internally so
  * we can safely ignore case and diacritical marks for the purposes of matching.
  */
-class Highlighter extends React.Component {
+class Highlighter extends React.PureComponent {
   _count = 0;
 
   render() {

commit 1567128e3605280450694ff2b0e27140cf0f5c95

Roll back breaking change introduced in 5c9444828ef2fc127fd3e6461a005938b7d6cd35

src/Highlighter.react.js
@@ -9,7 +9,7 @@ import {getMatchBounds} from './utils';
  * Results are already filtered by the time the component is used internally so
  * we can safely ignore case and diacritical marks for the purposes of matching.
  */
-class Highlighter extends React.PureComponent {
+class Highlighter extends React.Component {
   _count = 0;
 
   render() {