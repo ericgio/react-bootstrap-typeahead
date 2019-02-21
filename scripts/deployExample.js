bdef5dd98ea4982ab92d9431d7972c879531fdb4
Eric Giovanola <ericgio@gmail.com>
2019-02-20 23:16:17 -0800

    Don't publish example site for pre-release versions

--- scripts/deployExample.js
+++ scripts/deployExample.js
@@ -3,7 +3,19 @@
 const ghpages = require('gh-pages');
 const {version} = require('../package.json');
 
-ghpages.publish('example', {
-  message: `v${version}`,
-  src: '{index.html,package-example.js,assets/*}',
-});
+/**
+ * Don't publish pre-release versions, as denoted by the presence of a hyphen
+ * in the version number, (eg: 3.0.0-rc.1).
+ *
+ * See: https://semver.org/#spec-item-9
+ */
+if (version.split('-').length === 1) {
+  ghpages.publish('example', {
+    message: `v${version}`,
+    src: '{index.html,package-example.js,assets/*}',
+  });
+} else {
+  console.log(
+    `Skipped deploying examples for pre-release version: v${version}`
+  );
+}
