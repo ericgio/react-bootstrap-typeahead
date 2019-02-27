f6f430201b46e0af96e6bb7d578cee035f09fde7
Eric Giovanola <ericgio@gmail.com>
2019-02-27 14:05:19 -0800

    Flowify core components

--- src/core/TypeaheadInner.js
+++ src/core/TypeaheadInner.js
@@ -1,78 +1,42 @@
+// @flow
+
 import { pick } from 'lodash';
 import React from 'react';
 
-import { InputContext, MenuContext, TypeaheadContext } from './Context';
+import { InputContext, InputContextKeys, MenuContext, MenuContextKeys, TypeaheadContext, TypeaheadContextKeys } from './Context';
 import { getHintText, getInputText, getIsOnlyResult } from '../utils';
 import { RETURN } from '../constants';
 
-function getTypeaheadContextValue(props) {
-  const values = pick(props, [
-    'activeIndex',
-    'id',
-    'initialItem',
-    'onActiveItemChange',
-    'onAdd',
-    'onInitialItemChange',
-    'onMenuItemClick',
-    'selectHintOnEnter',
-  ]);
+import type { InputContextType, MenuContextType, TypeaheadContextType } from './Context';
+import type { TypeaheadInnerProps } from '../types';
 
+function getTypeaheadContextValue(
+  props: TypeaheadInnerProps
+): TypeaheadContextType {
   return {
-    ...values,
+    ...pick(props, TypeaheadContextKeys),
     hintText: getHintText(props),
     isOnlyResult: getIsOnlyResult(props),
   };
 }
 
-function getInputContextValue(props) {
-  const values = pick(props, [
-    'activeIndex',
-    'disabled',
-    'id',
-    'inputProps',
-    'inputRef',
-    'isFocused',
-    'isMenuShown',
-    'labelKey',
-    'multiple',
-    'onBlur',
-    'onChange',
-    'onFocus',
-    'onKeyDown',
-    'onRemove',
-    'placeholder',
-    'selected',
-  ]);
-
+function getInputContextValue(props: TypeaheadInnerProps): InputContextType {
   return {
-    ...values,
+    ...pick(props, InputContextKeys),
     ref: props.getReferenceElement,
     value: getInputText(props),
   };
 }
 
-function getMenuContextValue(props) {
-  const values = pick(props, [
-    'align',
-    'dropup',
-    'flip',
-    'id',
-    'labelKey',
-    'onMenuToggle',
-    'positionFixed',
-    'referenceElement',
-    'results',
-    'text',
-  ]);
-
+function getMenuContextValue(props: TypeaheadInnerProps): MenuContextType {
   return {
-    ...values,
+    ...pick(props, MenuContextKeys),
     show: props.isMenuShown,
   };
 }
 
-class TypeaheadInnerManager extends React.Component {
-  componentDidUpdate(prevProps, prevState) {
+class TypeaheadInnerManager extends React.Component<TypeaheadInnerProps> {
+  componentDidUpdate(prevProps: TypeaheadInnerProps) {
     const { allowNew, onInitialItemChange, results } = this.props;
 
     // Clear the initial item when there are no results.
@@ -98,12 +62,12 @@ class TypeaheadInnerManager extends React.Component {
     );
   }
 
-  _handleKeyDown = (e) => {
+  _handleKeyDown = (e: SyntheticKeyboardEvent<HTMLInputElement>) => {
     const { initialItem, onKeyDown, onAdd } = this.props;
 
     switch (e.keyCode) {
       case RETURN:
-        if (getIsOnlyResult(this.props)) {
+        if (initialItem && getIsOnlyResult(this.props)) {
           onAdd(initialItem);
         }
         break;
