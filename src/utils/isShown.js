893251ea571f6f4c35cac44a776f1c47e70515a2
Eric Giovanola <ericgio@gmail.com>
2019-02-27 11:43:59 -0800

    Remove ability to hide menu via falsy `emptyLabel` value

--- src/utils/isShown.js
+++ src/utils/isShown.js
@@ -1,18 +1,12 @@
-export default function isShown(results, props) {
-  const { emptyLabel, open, minLength, showMenu, text } = props;
-
+export default function isShown({ open, minLength, showMenu, text }) {
   // If menu visibility is controlled via props, that value takes precedence.
   if (open || open === false) {
     return open;
-  }
-
-  if (!showMenu) {
-    return false;
   }
 
   if (text.length < minLength) {
     return false;
   }
 
-  return !!(results.length || emptyLabel);
+  return showMenu;
 }
