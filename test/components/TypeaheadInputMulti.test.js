0eb5a208069d4d8f4f9ac65f577be688481e3e17
Eric Giovanola <ericgio@gmail.com>
2019-03-01 23:31:28 -0800

    Improve code coverage

--- test/components/TypeaheadInputMulti.test.js
+++ test/components/TypeaheadInputMulti.test.js
@@ -5,31 +5,35 @@ import React from 'react';
 import TypeaheadInputMulti from '../../src/components/TypeaheadInputMulti.react';
 
 import options from '../../example/exampleData';
-import { context, getFormControl, getHint, getInput, getTokens, TestInputProvider } from '../helpers';
+import { context, focus, getFormControl, getHint, getInput, getTokens, keyDown, TestInputProvider } from '../helpers';
+import { BACKSPACE, RETURN } from '../../src/constants';
+
+function isFocused(element) {
+  return element.getDOMNode() === document.activeElement;
+}
 
 describe('<TypeaheadInputMulti>', () => {
-  let text, wrapper;
+  let wrapper;
 
   beforeEach(() => {
-    text = 'text';
     wrapper = mount(
       <TestInputProvider
         {...context}
         inputProps={{}}
         labelKey="name"
         multiple
+        onFocus={noop}
+        onKeyDown={noop}
         options={options}
         selected={options.slice(1, 4)}
         selectHintOnEnter={false}
-        text={text}>
+        text="">
         {(props) => (
           <TypeaheadInputMulti
             {...props}
             inputRef={noop}
             onAdd={noop}
             onChange={noop}
-            onFocus={noop}
-            onKeyDown={noop}
           />
         )}
       </TestInputProvider>
@@ -45,6 +49,8 @@ describe('<TypeaheadInputMulti>', () => {
   });
 
   test('displays the selected text', () => {
+    const text = 'foo';
+    wrapper.setProps({ text });
     expect(getInput(wrapper).prop('value')).toBe(text);
   });
 
@@ -63,5 +69,59 @@ describe('<TypeaheadInputMulti>', () => {
     });
 
     expect(getHint(wrapper)).toBe(initialItem.name);
+  });
+
+  test('does not focus a disabled input', () => {
+    expect(isFocused(getInput(wrapper))).toBe(false);
+
+    wrapper.setProps({ disabled: true });
+    wrapper.simulate('click');
+
+    expect(isFocused(getInput(wrapper))).toBe(false);
+
+    wrapper.setProps({ disabled: false });
+    wrapper.simulate('click');
+
+    expect(isFocused(getInput(wrapper))).toBe(true);
+  });
+
+  describe('keydown handler', () => {
+    let onKeyDown;
+
+    beforeEach(() => {
+      onKeyDown = jest.fn();
+      wrapper.setProps({
+        onKeyDown,
+      });
+    });
+
+    test('calls the keydown handler', () => {
+      keyDown(wrapper, RETURN);
+      expect(onKeyDown).toHaveBeenCalledTimes(1);
+    });
+
+    test('focuses the last token', () => {
+      const input = getInput(wrapper);
+
+      focus(wrapper);
+      expect(input.getDOMNode()).toEqual(document.activeElement);
+
+      keyDown(wrapper, BACKSPACE);
+
+      expect(isFocused(getTokens(wrapper).last())).toBe(true);
+      expect(onKeyDown).toHaveBeenCalledTimes(1);
+    });
+
+    test('does not focus the last token when the input has a value', () => {
+      wrapper.setProps({
+        text: 'foo',
+      });
+
+      focus(wrapper);
+      keyDown(wrapper, BACKSPACE);
+
+      expect(isFocused(getInput(wrapper))).toBe(true);
+      expect(onKeyDown).toHaveBeenCalledTimes(1);
+    });
   });
 });
