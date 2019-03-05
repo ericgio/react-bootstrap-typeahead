17d540be5405d4237dc6f460711be50814b70bf1
Eric Giovanola <ericgio@gmail.com>
2019-03-05 00:36:58 -0800

    Consolidate propTypes

--- src/propTypes/optionType.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.oneOfType([
-  PropTypes.arrayOf(PropTypes.object.isRequired),
-  PropTypes.arrayOf(PropTypes.string.isRequired),
-]);
