e6b17d75ed92ee3221c5f2138e417d585b575e6b
Eric Giovanola <ericgio@gmail.com>
2019-03-12 16:23:38 -0700

    Update Markdown example component

--- example/components/Markdown.react.js
+++ example/components/Markdown.react.js
@@ -3,32 +3,29 @@
 
 import cx from 'classnames';
 import marked from 'marked';
+import PropTypes from 'prop-types';
 import React from 'react';
 
-class Markdown extends React.Component {
-  componentDidMount() {
-    marked.setOptions({
-      breaks: true,
-      gfm: true,
-      highlight: (code) => Prism.highlight(code, Prism.languages.markdown),
-      pedantic: false,
-      sanitize: true,
-      smartLists: true,
-      smartypants: false,
-      tables: true,
-    });
-  }
+marked.setOptions({
+  breaks: true,
+  gfm: true,
+  highlight: (code) => Prism.highlight(code, Prism.languages.markdown),
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false,
+  tables: true,
+});
 
-  render() {
-    const { children, className } = this.props;
+const Markdown = ({ children, className }) => (
+  <div
+    className={cx('markdown-body', className)}
+    dangerouslySetInnerHTML={{ __html: marked.parse(children) }}
+  />
+);
 
-    return (
-      <div
-        className={cx('markdown-body', className)}
-        dangerouslySetInnerHTML={{ __html: marked.parse(children) }}
-      />
-    );
-  }
-}
+Markdown.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default Markdown;
