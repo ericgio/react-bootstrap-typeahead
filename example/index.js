74a79513e0e3632ff9467d393efcdfc12f1fa935
Eric Giovanola <ericgio@gmail.com>
2019-03-14 17:21:25 -0700

    Update CSS build script

--- example/index.js
+++ example/index.js
@@ -11,7 +11,7 @@ import FilteringSection from './sections/FilteringSection.react';
 import PublicMethodsSection from './sections/PublicMethodsSection.react';
 import RenderingSection from './sections/RenderingSection.react';
 
-import '../scss/Typeahead.scss';
+import '../styles/Typeahead.scss';
 
 render(
   <Page title="Examples">
