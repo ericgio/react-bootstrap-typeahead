71cf9c2a345c4450020c6efeffaf6fd0402d0603
Eric Giovanola <ericgio@gmail.com>
2019-03-19 12:49:16 -0700

    Use Rollup for build & include es files

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,15 +1,65 @@
-const config = require('../webpack.config.base');
 const path = require('path');
 
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = (env, argv) => {
-  return Object.assign(config, {
+  return {
     entry: './example/index.js',
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.js$/,
+          use: [
+            'babel-loader',
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            output: {
+              comments: false,
+            },
+          },
+        }),
+      ],
+    },
     output: {
       filename: 'package-example.js',
       path: path.resolve('example'),
     },
+    plugins: [
+      new CircularDependencyPlugin({
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+        exclude: /node_modules/,
+        failOnError: true,
+      }),
+    ],
+    resolve: {
+      extensions: ['.js'],
+    },
     stats: {
       warnings: argv.mode !== 'production',
     },
-  });
+  };
 };
