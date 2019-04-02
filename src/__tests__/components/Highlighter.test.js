76f746d06b61491c10c632198b94e425e2acc6ed
Eric Giovanola <ericgio@gmail.com>
2019-04-02 01:34:48 -0700

    Update/add a few tests

--- src/__tests__/components/Highlighter.test.js
+++ src/__tests__/components/Highlighter.test.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 import Highlighter from '../../components/Highlighter.react';
 
+function getMatches(wrapper) {
+  return wrapper.find('mark');
+}
+
 describe('<Highlighter>', () => {
   let highlighter;
 
@@ -16,12 +20,12 @@ describe('<Highlighter>', () => {
 
   test('does not highlight text when there is no search string', () => {
     expect(highlighter.text()).toBe('California');
-    expect(highlighter.find('mark')).toHaveLength(0);
+    expect(getMatches(highlighter)).toHaveLength(0);
   });
 
   test('does not highlight text when there is no match', () => {
     highlighter.setProps({ search: 'x' });
-    expect(highlighter.find('mark')).toHaveLength(0);
+    expect(getMatches(highlighter)).toHaveLength(0);
   });
 
   test('handles an empty child string', () => {
@@ -33,17 +37,29 @@ describe('<Highlighter>', () => {
     expect(highlighter.text()).toBe('');
   });
 
-  test('correctly highlights text', () => {
+  test('highlights text in the middle of a string', () => {
     highlighter.setProps({ search: 'i' });
 
     // Output: [Cal, <mark>i</mark>, forn, <mark>i</mark>, a]
     expect(highlighter.length).toBe(5);
     expect(highlighter.first().text()).toBe('Cal');
 
-    const matches = highlighter.find('mark');
+    const matches = getMatches(highlighter);
     expect(matches.length).toBe(2);
     expect(matches.first().text()).toBe('i');
     expect(matches.first().hasClass('rbt-highlight-text')).toBe(true);
+  });
+
+  test('highlights text at the beginning of a string', () => {
+    highlighter.setProps({ search: 'cal' });
+
+    // Output: [<mark>Cal</mark>, ifornia]
+    expect(highlighter.length).toBe(2);
+    expect(highlighter.first().text()).toBe('Cal');
+
+    const matches = getMatches(highlighter);
+    expect(matches.length).toBe(1);
+    expect(matches.first().text()).toBe('Cal');
   });
 
   test('adds custom classnames to the highlighted children', () => {
@@ -52,6 +68,6 @@ describe('<Highlighter>', () => {
       search: 'i',
     });
 
-    expect(highlighter.find('mark').first().hasClass('foo')).toBe(true);
+    expect(getMatches(highlighter).first().hasClass('foo')).toBe(true);
   });
 });
