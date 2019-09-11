0cf70b6f715521cc10e1dbbea2fa153c7e2783fb
Eric Giovanola <ericgio@gmail.com>
2019-09-11 11:02:40 -0700

    Update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = (config) => {
     customLaunchers: {
       Chrome_travis_ci: {
         base: 'Chrome',
-        flags: ['--no-sandbox'],
+        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     },
     files: ['test/index.js'],
