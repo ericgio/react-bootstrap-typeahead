a1422b0361664b4f5f139e11f9d8a60767895629
Eric Giovanola <ericgio@gmail.com>
2019-10-19 16:03:08 -0700

    Export lower-level input components in main file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // Components
 export AsyncTypeahead from './components/AsyncTypeahead.react';
+export ClearButton from './components/ClearButton.react';
 export Highlighter from './components/Highlighter.react';
+export Input from './components/Input.react';
+export Loader from './components/Loader.react';
 export Menu from './components/Menu.react';
 export MenuItem from './components/MenuItem.react';
 export Token from './components/Token.react';
 export Typeahead from './components/Typeahead.react';
+export TypeaheadInputMulti from './components/TypeaheadInputMulti.react';
+export TypeaheadInputSingle from './components/TypeaheadInputSingle.react';
 export TypeaheadMenu from './components/TypeaheadMenu.react';
 
 // HOCs
