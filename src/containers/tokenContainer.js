6c0834a960641bad039e4b657f15cd6d0df2d29e
Eric Giovanola <ericgio@gmail.com>
2020-06-01 01:56:19 -0700

    `useToken` hook

--- src/containers/tokenContainer.js
+++ src/containers/tokenContainer.js
@@ -1,26 +1,21 @@
 // @flow
 
 import PropTypes from 'prop-types';
-import React, { type ComponentType } from 'react';
+import React, { type ComponentType, useState } from 'react';
+import { useRootClose } from 'react-overlays';
 
-import RootClose from '../components/RootClose.react';
-
-import { getDisplayName, isFunction, noop } from '../utils';
+import { getDisplayName, isFunction } from '../utils';
 import { BACKSPACE } from '../constants';
 
 import { optionType } from '../propTypes';
 import type { EventHandler, Option, OptionHandler } from '../types';
 
 type Props = {
-  onBlur: EventHandler<HTMLElement>,
-  onClick: EventHandler<HTMLElement>,
-  onFocus: EventHandler<HTMLElement>,
+  onBlur?: EventHandler<HTMLElement>,
+  onClick?: EventHandler<HTMLElement>,
+  onFocus?: EventHandler<HTMLElement>,
   onRemove?: OptionHandler,
   option: Option,
-};
-
-type State = {
-  active: boolean,
 };
 
 const propTypes = {
@@ -31,98 +26,87 @@ const propTypes = {
   option: optionType.isRequired,
 };
 
-const defaultProps = {
-  onBlur: noop,
-  onClick: noop,
-  onFocus: noop,
+export const useToken = ({
+  onBlur,
+  onClick,
+  onFocus,
+  onRemove,
+  option,
+  ...props
+}: Props) => {
+  const [active, setActive] = useState<boolean>(false);
+  const [rootElement, attachRef] = useState<mixed>(null);
+
+  const handleActiveChange = (
+    e: SyntheticEvent<HTMLElement>,
+    isActive: boolean,
+    callback?: EventHandler<HTMLElement>,
+  ) => {
+    e.stopPropagation();
+    setActive(isActive);
+    typeof callback === 'function' && callback(e);
+  };
+
+  const handleBlur = (e: SyntheticEvent<HTMLElement>) => {
+    handleActiveChange(e, false, onBlur);
+  };
+
+  const handleClick = (e: SyntheticEvent<HTMLElement>) => {
+    handleActiveChange(e, true, onClick);
+  };
+
+  const handleFocus = (e: SyntheticEvent<HTMLElement>) => {
+    handleActiveChange(e, true, onFocus);
+  };
+
+  const handleRemove = () => {
+    // Flow having trouble with `isFunction` here for some reason...
+    if (typeof onRemove === 'function') {
+      onRemove(option);
+    }
+  };
+
+  const handleKeyDown = (e: SyntheticKeyboardEvent<HTMLInputElement>) => {
+    switch (e.keyCode) {
+      case BACKSPACE:
+        if (active) {
+          // Prevent backspace keypress from triggering the browser "back"
+          // action.
+          e.preventDefault();
+          handleRemove();
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  useRootClose(rootElement, handleBlur, {
+    ...props,
+    disabled: !active,
+  });
+
+  return {
+    ...props,
+    active,
+    onBlur: handleBlur,
+    onClick: handleClick,
+    onFocus: handleFocus,
+    onKeyDown: handleKeyDown,
+    onRemove: isFunction(onRemove) ? handleRemove : undefined,
+    ref: attachRef,
+  };
 };
 
-/**
- * Higher-order component to encapsulate Token behaviors.
- */
 const tokenContainer = (Component: ComponentType<*>) => {
-  class WrappedComponent extends React.Component<Props, State> {
-    static displayName = `tokenContainer(${getDisplayName(Component)})`;
-    static propTypes = propTypes;
-    static defaultProps = defaultProps;
+  const displayName = `tokenContainer(${getDisplayName(Component)})`;
 
-    state = {
-      active: false,
-    };
+  const WrappedToken = (props: Props) => <Component {...useToken(props)} />;
 
-    render() {
-      const { onRemove } = this.props;
-      const { active } = this.state;
+  WrappedToken.displayName = displayName;
+  WrappedToken.propTypes = propTypes;
 
-      return (
-        <RootClose
-          disabled={!active}
-          onRootClose={this._handleBlur}>
-          {(ref) => (
-            <Component
-              {...this.props}
-              active={active}
-              onBlur={this._handleBlur}
-              onClick={this._handleClick}
-              onFocus={this._handleFocus}
-              onKeyDown={this._handleKeyDown}
-              onRemove={isFunction(onRemove) ? this._handleRemove : undefined}
-              ref={ref}
-            />
-          )}
-        </RootClose>
-      );
-    }
-
-    _handleActiveChange = (
-      e: SyntheticEvent<HTMLElement>,
-      active: boolean,
-      callback: EventHandler<HTMLElement>,
-    ) => {
-      // e.persist() isn't always present.
-      e.persist && e.persist();
-      e.stopPropagation();
-      this.setState({ active }, () => callback(e));
-    }
-
-    _handleBlur = (e: SyntheticEvent<HTMLElement>) => {
-      this._handleActiveChange(e, false, this.props.onBlur);
-    }
-
-    _handleClick = (e: SyntheticEvent<HTMLElement>) => {
-      this._handleActiveChange(e, true, this.props.onClick);
-    }
-
-    _handleFocus = (e: SyntheticEvent<HTMLElement>) => {
-      this._handleActiveChange(e, true, this.props.onFocus);
-    }
-
-    _handleKeyDown = (e: SyntheticKeyboardEvent<HTMLInputElement>) => {
-      switch (e.keyCode) {
-        case BACKSPACE:
-          if (this.state.active) {
-            // Prevent backspace keypress from triggering the browser "back"
-            // action.
-            e.preventDefault();
-            this._handleRemove();
-          }
-          break;
-        default:
-          break;
-      }
-    }
-
-    _handleRemove = () => {
-      const { onRemove, option } = this.props;
-
-      // Flow having trouble with `isFunction` here for some reason...
-      if (typeof onRemove === 'function') {
-        onRemove(option);
-      }
-    }
-  }
-
-  return WrappedComponent;
+  return WrappedToken;
 };
 
 export default tokenContainer;
