295ad4e9a10790f668374fa3aaabc2e2cefd60d7
Eric Giovanola <ericgio@gmail.com>
2022-02-04 12:36:20 -0800

    Update Jest config/setup

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/example',
     '<rootDir>/src/propTypes',
   ],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules/'],
-  setupFilesAfterEnv: [
-    '@testing-library/jest-dom/extend-expect',
-    './jest.setup.js',
-  ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: [
+    '/node_modules',
+    '/cjs',
+    '/es',
+    '/example',
+    '/types',
+  ],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,4 @@
 import { toHaveNoViolations } from 'jest-axe';
+import '@testing-library/jest-dom/extend-expect';
 
 expect.extend(toHaveNoViolations);
