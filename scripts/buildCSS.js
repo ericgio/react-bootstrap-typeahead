cb2620f9d11c0db6efc42a501658c6a15d5a4d54
Eric Giovanola <ericgio@gmail.com>
2022-02-22 00:34:53 -0800

    Use `sass.compile` instead of `sass.render`

--- scripts/buildCSS.js
+++ scripts/buildCSS.js
@@ -13,32 +13,12 @@ const ROOT = path.join(__dirname, '..');
 const OUT_DIR = path.join(ROOT, 'css');
 const STYLES_DIR = path.join(ROOT, 'styles');
 
-function buildCSS(options) {
-  // Get the base filename.
-  let filename = options.file.split(/\/|\\/).pop().replace('.scss', '');
-
-  // Denote minified CSS.
-  if (options.outputStyle === 'compressed') {
-    filename = `${filename}.min`;
-  }
-
-  // Render CSS files.
-  sass.render(options, (err, result) => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-
-    fs.writeFileSync(path.join(OUT_DIR, `${filename}.css`), result.css);
-  });
-}
+console.log(chalk.cyan('Building CSS files...\n'));
 
 // Create the output directory if it doesn't exist.
 if (!fs.existsSync(OUT_DIR)) {
   fs.mkdirSync(OUT_DIR);
 }
-
-console.log(chalk.cyan('Building CSS files...\n'));
 
 fs.readdirSync(STYLES_DIR).forEach((filename) => {
   const file = path.join(STYLES_DIR, filename);
@@ -47,10 +27,18 @@ fs.readdirSync(STYLES_DIR).forEach((filename) => {
   fs.copyFileSync(file, path.join(OUT_DIR, filename));
 
   // Output both expanded and minified versions.
-  ['compressed', 'expanded'].forEach((outputStyle) => {
-    buildCSS({
-      file,
-      outputStyle,
-    });
+  ['compressed', 'expanded'].forEach((style) => {
+    // Get the base filename.
+    let name = filename.replace('.scss', '');
+    if (style === 'compressed') {
+      // Denote minified CSS.
+      name += '.min';
+    }
+
+    const result = sass.compile(file, { style });
+
+    fs.writeFileSync(path.join(OUT_DIR, `${name}.css`), result.css);
   });
 });
+
+console.log(chalk.cyan('Done.\n'));
