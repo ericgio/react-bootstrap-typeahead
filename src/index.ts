fd8b23eae1810e04d605323eca3b89440ce7b27c
Eric Giovanola <ericgio@gmail.com>
2023-05-18 01:26:45 -0700

    Expose types at top level

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,23 @@ export { default as Hint, useHint } from './components/Hint';
 export { default as Input } from './components/Input';
 export { default as Loader } from './components/Loader';
 export { default as Menu } from './components/Menu';
+export * from './components/Menu';
 export { default as MenuItem } from './components/MenuItem';
+export * from './components/MenuItem';
 export { default as Token } from './components/Token';
+export * from './components/Token';
 export { default as Typeahead } from './components/Typeahead';
+export * from './components/Typeahead';
 export { default as TypeaheadInputMulti } from './components/TypeaheadInputMulti';
 export { default as TypeaheadInputSingle } from './components/TypeaheadInputSingle';
 export { default as TypeaheadMenu } from './components/TypeaheadMenu';
+export * from './components/TypeaheadMenu';
 
 // HOCs + Hooks
 export * from './behaviors/async';
 export * from './behaviors/item';
 export * from './behaviors/token';
+
+// Types
+export { default as TypeaheadRef } from './core/Typeahead';
+export * from './types';
