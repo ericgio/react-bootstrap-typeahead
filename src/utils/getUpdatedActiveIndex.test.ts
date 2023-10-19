45e490d6089cea22d506d443f3bfd0591d8ef26c
Daniel Leavitt <daniel.leavitt@gmail.com>
2023-10-19 13:57:03 -0700

    don't skip options with a falsey disabled value (#819)
    Thanks @dleavitt!

--- src/utils/getUpdatedActiveIndex.test.ts
+++ src/utils/getUpdatedActiveIndex.test.ts
@@ -7,7 +7,8 @@ const options = [
   { name: 'foo' },
   { disabled: true, name: 'bar' },
   { disabled: true, name: 'boo' },
-  { name: 'baz' },
+  { name: 'baz', disabled: false },
+  { name: 'bja', disabled: undefined },
 ];
 
 const stringOptions = ['foo', 'bar', 'baz'];
@@ -15,9 +16,11 @@ const stringOptions = ['foo', 'bar', 'baz'];
 test('getUpdatedActiveIndex', () => {
   expect(getUpdatedActiveIndex(-1, 'ArrowDown', options)).toBe(0);
   expect(getUpdatedActiveIndex(0, 'ArrowDown', options)).toBe(3);
-  expect(getUpdatedActiveIndex(3, 'ArrowDown', options)).toBe(-1);
+  expect(getUpdatedActiveIndex(3, 'ArrowDown', options)).toBe(4);
+  expect(getUpdatedActiveIndex(4, 'ArrowDown', options)).toBe(-1);
 
-  expect(getUpdatedActiveIndex(-1, 'ArrowUp', options)).toBe(3);
+  expect(getUpdatedActiveIndex(-1, 'ArrowUp', options)).toBe(4);
+  expect(getUpdatedActiveIndex(4, 'ArrowUp', options)).toBe(3);
   expect(getUpdatedActiveIndex(3, 'ArrowUp', options)).toBe(0);
   expect(getUpdatedActiveIndex(0, 'ArrowUp', options)).toBe(-1);
 });
@@ -36,6 +39,8 @@ test('skipDisabledOptions', () => {
 test('isDisabledOption', () => {
   expect(isDisabledOption(0, options)).toBe(false);
   expect(isDisabledOption(1, options)).toBe(true);
+  expect(isDisabledOption(3, options)).toBe(false);
+  expect(isDisabledOption(4, options)).toBe(false);
   expect(isDisabledOption(6, options)).toBe(false);
   expect(isDisabledOption(0, stringOptions)).toBe(false);
 });
