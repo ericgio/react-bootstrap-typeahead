3a5e1ff26601435bdcb7073109fdfc86322c237c
Eric Giovanola <ericgio@gmail.com>
2023-10-20 13:38:36 -0700

    Fix prettier errors

--- src/utils/hasOwnProperty.ts
+++ src/utils/hasOwnProperty.ts
@@ -3,7 +3,7 @@
  */
 export default function hasOwnProperty<
   X extends Record<string, unknown>,
-  Y extends PropertyKey
+  Y extends PropertyKey,
 >(obj: X, prop: Y): obj is X & Record<Y, unknown> {
   return Object.prototype.hasOwnProperty.call(obj, prop);
 }
