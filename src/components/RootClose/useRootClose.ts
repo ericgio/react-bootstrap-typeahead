e3fd95a567394f4ab92fcc3a16878a71f1785579
Eric Giovanola <ericgio@gmail.com>
2025-03-18 23:13:13 -0700

    Use @restart/ui for root close

--- src/components/RootClose/useRootClose.ts
+++ src/components/RootClose/useRootClose.ts
@@ -1,50 +1,12 @@
-import { useEffect, useRef } from 'react';
-import useEventCallback from '@restart/hooks/useEventCallback';
-
-import useClickOutside, { ClickOutsideOptions } from './useClickOutside';
-import { noop } from '../../utils';
+import { useRef } from 'react';
+import _useRootClose, { RootCloseOptions } from '@restart/ui/useRootClose';
 
 function useRootClose(
   onRootClose: (e: Event) => void,
-  options: ClickOutsideOptions
+  options: RootCloseOptions
 ) {
   const ref = useRef<HTMLDivElement>(null);
-
-  const onClose = onRootClose || noop;
-
-  useClickOutside(ref, onClose, options);
-
-  const handleKeyUp = useEventCallback((e: KeyboardEvent) => {
-    if (e.key === 'Escape') {
-      onClose(e);
-    }
-  });
-
-  useEffect(() => {
-    if (options.disabled || ref == null) return undefined;
-
-    const doc = ref.current?.ownerDocument || document;
-
-    // Store the current event to avoid triggering handlers immediately
-    // https://github.com/facebook/react/issues/20074
-    let currentEvent = (doc.defaultView || window).event;
-
-    const onKeyUp = (e: KeyboardEvent) => {
-      // skip if this event is the same as the one running when we added the handlers
-      if (e === currentEvent) {
-        currentEvent = undefined;
-        return;
-      }
-      handleKeyUp(e);
-    };
-
-    doc.addEventListener('keyup', onKeyUp);
-
-    return () => {
-      doc.removeEventListener('keyup', onKeyUp);
-    };
-  }, [ref, options.disabled, handleKeyUp]);
-
+  _useRootClose(ref, onRootClose, options);
   return ref;
 }
 
